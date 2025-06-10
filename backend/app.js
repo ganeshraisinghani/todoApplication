ceb7c128e7af3d58753d6211949ed250867c120c
ganesh <ganeshmohan8297@gmail.com>
2025-06-10 11:27:43 +0530

    Minor changes commit!

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const todoRoutes = require('./routes/todo');
+require('dotenv').config();
 
 const app = express();
+
+// Middleware
 app.use(express.json());
+
+// Routes
 app.use('/api/todos', todoRoutes);
+
+// MongoDB connection (no deprecated options)
+if (process.env.DB_URI) {
+  mongoose.connect(process.env.DB_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+} else {
+  console.error('DB_URI is not defined in .env');
+}
 
 module.exports = app;
